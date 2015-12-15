8d43cef83c9ca44846a96100e7d63212a38989c4
Félix Gagnon-Grenier <fractalesque@gmail.com>
2015-12-15 18:19:48 -0500

    update tests

--- tests/StorageTest.php
+++ tests/StorageTest.php
@@ -9,18 +9,18 @@ class StorageTest extends \Tests\BaseStorageCase
     }
     public function testWithoutTrailingSlash()
     {
-        $loader = new \Storage\FileLoader(__DIR__.'/test_files');
-        $string = $loader->getAsString('dummy.txt');
+        $loader = new \Storage\FileLoader(__DIR__.'/test_files','txt');
+        $string = $loader->getAsString('dummy');
         $this->assertInternalType('string',$string);
     }
     public function testLoad()
     {
-        $loader = new \Storage\FileLoader(__DIR__.'/test_files/');
-        $string = $loader->getAsString('dummy.txt');
+        $loader = new \Storage\FileLoader(__DIR__.'/test_files/','txt');
+        $string = $loader->getAsString('dummy');
         $this->assertInternalType('string',$string);
         $this->assertContains('test string',$string);
 
-        $file = $loader->load('dummy.txt');
+        $file = $loader->load('dummy');
         ob_start();
         require $file;
         $string = ob_get_clean();
@@ -38,5 +38,11 @@ class StorageTest extends \Tests\BaseStorageCase
         $loader = new \Storage\FileLoader(__DIR__.'/test_files/','.txt');
         $string = $loader->getAsString('dummy');
         $this->assertInternalType('string',$string);
+    }
+    public function testLoaderWithoutSpecifyingExtension()
+    {
+        $loader = new \Storage\FileLoader(__DIR__.'/test_files/');
+        $file = $loader->load("dummy.txt");
+        $this->assertInstanceOf("\\Storage\\File",$file);
     }
 }
