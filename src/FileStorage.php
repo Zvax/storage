9bee5bb360dcbf60b3789f34b51480227b1eb979
Félix Gagnon-Grenier <fractalesque@gmail.com>
2022-05-22 08:03:55 -0400

    [bugfix] add coverage over FileStorage

--- src/FileStorage.php
+++ src/FileStorage.php
@@ -2,40 +2,50 @@
 
 namespace Storage;
 
-class FileStorage implements Storage {
-
-    private $root;
-
-    public function __construct($root = __DIR__.'/data/images/') {
-        if (!is_dir($root)) mkdir($root);
-        $this->root = $root;
+class FileStorage implements Storage
+{
+    public function __construct(private string $root)
+    {
+        if (!is_dir($root)) {
+            // todo maybe sanely
+            mkdir($root);
+        }
     }
 
-    public function offsetExists($offset) {
-        return file_exists("$this->root$offset");
+    public function offsetExists($offset): bool
+    {
+        return file_exists("$this->root/$offset");
     }
 
-    public function offsetGet($offset) {
-        return file_get_contents("$this->root$offset");
+    public function offsetGet($offset): mixed
+    {
+        return file_get_contents("$this->root/$offset");
     }
 
-    public function offsetSet($offset, $value) {
-        if (!is_dir($this->pathify($offset))) mkdir($this->pathify($offset),0755,true);
-        file_put_contents("$this->root$offset",$value);
+    public function offsetSet($offset, $value): void
+    {
+        if (!is_dir($this->pathify($offset))) {
+            mkdir($this->pathify($offset), 0755, true);
+        }
+        file_put_contents("$this->root/$offset", $value);
     }
 
-    public function offsetUnset($offset) {
-        unlink("$this->root$offset");
+    public function offsetUnset($offset): void
+    {
+        unlink("$this->root/$offset");
     }
 
-    public function getPath($offset) {
-        return "$this->root$offset";
+    public function getPath($offset): string
+    {
+        return "$this->root/$offset";
     }
 
-    private function pathify($offset) {
-        $bits = explode('/',"$this->root$offset");
+    private function pathify($offset): string
+    {
+        $bits = explode('/', "$this->root/$offset");
+        // wtf am i even doing
         array_pop($bits);
-        return implode('/',$bits);
+        return implode('/', $bits);
     }
 
 }
